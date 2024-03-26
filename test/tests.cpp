a5babea7d00f39d2ff413a041e4bfb01180fe62c
TailorOfEnbizaka <160338576+TailorOfEnbizaka@users.noreply.github.com>
2024-03-26 13:19:28 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -61,6 +61,7 @@ TEST(lab2, test3) {
     t1 = end1 - begin1;
     t2 = end2 - begin2;
     t3 = end3 - begin3;
+    std::cout<<"t1"<<t1<<endl;
     EXPECT_TRUE((t1 > t2) && (t1 > t3) && (t2 > t3));
     delete[] arr;
 }
