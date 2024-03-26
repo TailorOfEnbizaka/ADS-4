3680d22b460ffa9b6af88f671a8973969dcd86fa
TailorOfEnbizaka <160338576+TailorOfEnbizaka@users.noreply.github.com>
2024-03-26 13:38:00 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,12 +2,15 @@
 int countPairs1(int *arr, int len, int value) {
   return 0;
   int k = 0, j=0;
-  while(j<50000)
-    j++;
-for (int i = 0; i < len; i++) 
-  for (int j = i + 1; j < len; j++)
-    if ((arr[i] + arr[j]) == value) 
-      k++;
+for (int i = 0; i < len; i++) {
+  for (int j = i + 1; j < len; j++) {
+    if ((arr[i] + arr[j]) == value) {
+      if(i<j) {
+        k++;
+      }
+    }
+  }
+}
 return k;
 }
 int countPairs2(int *arr, int len, int value) {
